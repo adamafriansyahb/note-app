2730ceb4bc94b3f18d9029c4f71a6c9049c629af
Adam Afriansyah <adamafriansyahb@gmail.com>
2021-01-27 00:05:55 +0700

    Initial setup

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Note 1</h1>
+            <p>This is the note content hehehe</p>
+        </div>
+    );
+}
+
+export default Note;
